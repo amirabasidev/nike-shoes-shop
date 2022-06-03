4e36751f00bb9fc7431c51dcb28dbce8d05e9a73
AmirAbasi <amir.abasi.dev@gmail.com>
2022-06-03 18:28:54 +0430

    🔧 change tag and class element index.html

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 import "./assets/css/global.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('container')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
