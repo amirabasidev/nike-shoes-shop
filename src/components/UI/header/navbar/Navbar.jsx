1c9fb0d8f5c05a13efa0efe93d51b5667ce7f1b5
AmirAbasi <amir.abasi.dev@gmail.com>
2022-06-03 23:02:24 +0430

    style : 💄 responsive navbar

--- src/components/UI/header/navbar/Navbar.jsx
+++ src/components/UI/header/navbar/Navbar.jsx
@@ -1,28 +1,48 @@
+import { Menu } from "iconsax-react";
 import { NavLink } from "react-router-dom";
 
 import navbarItems from "../../../../config/navbarItems";
+import useToggle from "../../../../hooks/useToggle";
 
 import classes from "./navbar.module.css";
 
 const Navbar = () => {
+  const [navbar__Ref, openNavbar, closeNavbar, ContainerToggle] = useToggle(
+    classes.navbar__show,
+  );
+
   return (
-    <nav className={classes.navbar}>
-      <ul className={classes.navbar__ul}>
-        {navbarItems.map(({ key, title, url }) => {
-          const isActiveClass = ({ isActive }) =>
-            isActive
-              ? `${classes.navbar__link} ${classes.navbar__link_active}`
-              : classes.navbar__link;
-          return (
-            <li className={classes.navbar__item} key={key}>
-              <NavLink className={isActiveClass} to={url}>
-                {title}
-              </NavLink>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
+    <>
+      <button className={`icon ${classes.navbar__open}`} onClick={openNavbar}>
+        <Menu size="100%" />
+      </button>
+      <ContainerToggle>
+        <nav className={classes.navbar} ref={navbar__Ref}>
+          <div className={classes.navbar__logo}>
+            <img src="/src/assets/images/logo__nav.jpg" alt="logo__navbar" />
+          </div>
+          <ul className={classes.navbar__ul}>
+            {navbarItems.map(({ key, title, url }) => {
+              const isActiveClass = ({ isActive }) =>
+                isActive
+                  ? `${classes.navbar__link} ${classes.navbar__link_active}`
+                  : classes.navbar__link;
+              return (
+                <li
+                  onClick={closeNavbar}
+                  className={classes.navbar__item}
+                  key={key}
+                >
+                  <NavLink className={isActiveClass} to={url}>
+                    {title}
+                  </NavLink>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </ContainerToggle>
+    </>
   );
 };
 
