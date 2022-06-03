206a4762d71bbaa4e6b887177f33b3418a6c7eb8
AmirAbasi <amir.abasi.dev@gmail.com>
2022-06-03 18:39:40 +0430

    refactor : ♻ config navbar Items

---
+++ src/config/navbarItems.js
@@ -0,0 +1,8 @@
+const navbarItems = [
+  { title: "Home", url: "/", key: 1 },
+  { title: "Products", url: "/products", key: 2 },
+  { title: "About", url: "/about", key: 3 },
+  { title: "Contact", url: "/contact", key: 4 },
+];
+
+export default navbarItems
