fd853062db9d8f13e92982a4d5c51020cfd63616
AmirAbasi <amir.abasi.dev@gmail.com>
2022-06-07 23:03:13 +0430

    feat : ✨ add productsSlider to page Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,18 @@
 import Banner from "../../components/UI/banner/Banner";
+import ProductsSlider from "../../components/productsSlider/ProductsSlider";
 
 const Home = () => {
   return (
     <section>
       <Banner />
+      <ProductsSlider
+        title="last products"
+        url="https://amirabasinasab.ir/nike/index.php/productitems"
+      />
+      <ProductsSlider
+        title="joradn Shoes"
+        url="https://amirabasinasab.ir/nike/index.php/productitems?categoryName='jordan'"
+      />
     </section>
   );
 };
