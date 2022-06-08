0c758948a17e60c8357f0aaf0d68c8aa614bd6c1
AmirAbasi <amir.abasi.dev@gmail.com>
2022-06-08 22:37:51 +0430

    refactor : ♻ config categories and social

---
+++ src/config/categories.js
@@ -0,0 +1,8 @@
+const categories = [
+  { key: 1, title: "jordan", url: "/category/jordan" },
+  { key: 2, title: "lifestyle ", url: "/category/lifestyle " },
+  { key: 3, title: "basketball", url: "/category/basketball " },
+  { key: 4, title: "soccer", url: "/category/soccer" },
+];
+
+export default categories
---
+++ src/config/social.js
@@ -0,0 +1,10 @@
+import { Instagram,Youtube, Facebook ,Whatsapp} from "iconsax-react";
+
+const social = [
+    { key: 1, Icon: Instagram, url: "/" },
+    { key: 2, Icon: Youtube, url: "/" },
+    { key: 3, Icon: Facebook, url: "/" },
+    { key: 4, Icon: Whatsapp, url: "/" },
+];
+
+export default social;
