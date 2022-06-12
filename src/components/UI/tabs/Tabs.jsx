75cac98db10f2a2ee60c54d8d3c845b5b77cf870
AmirAbasi <amir.abasi.dev@gmail.com>
2022-06-12 23:07:27 +0430

    feat : ✨ create component tabs

---
+++ src/components/UI/tabs/Tabs.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+import TabTitle from "./tabTitle/TabTitle";
+
+import classes from "./tabs.module.css";
+
+const Tabs = ({ children }) => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  return (
+    <section className={classes.tabs}>
+      <ul className={classes.tabs__items}>
+        {children.map(({ props }, index) => (
+          <TabTitle
+            key={index}
+            index={index}
+            icon={props.icon}
+            title={props.title}
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
+        ))}
+      </ul>
+      <span
+        style={{ transform: `translateX(${activeTab * 150}px)` }}
+        className={classes.border}
+      ></span>
+      {children[activeTab]}
+    </section>
+  );
+};
+
+export default Tabs;
