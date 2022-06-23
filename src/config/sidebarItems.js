a4e242ef119f3248b8941598ebfbfcfb9b73e048
AmirAbasi <amir.abasi.dev@gmail.com>
2022-06-23 11:36:28 +0430

    refactor : ♻ cnofig siderbar items

---
+++ src/config/sidebarItems.js
@@ -0,0 +1,9 @@
+import { Element, ReceiptText, ProfileCircle } from "iconsax-react";
+
+const sidebarItems = [
+  { key: 1, title: "Dashboard", url: "/panel", icon: Element },
+  { key: 2, title: "Orders", url: "/panel/orders", icon: ReceiptText },
+  { key: 3, title: "Profile", url: "/panel/profile", icon: ProfileCircle },
+];
+
+export default sidebarItems;
