e03abcc263198e08106611367abec426e4ea5dae
AmirAbasi <amir.abasi.dev@gmail.com>
2022-06-25 22:05:05 +0430

    style : 💄 responsive panel

--- src/components/panel/sidebar/Sidebar.jsx
+++ src/components/panel/sidebar/Sidebar.jsx
@@ -1,8 +1,9 @@
 import { createElement } from "react";
 import { useDispatch } from "react-redux";
-import { LogoutCurve } from "iconsax-react";
+import { LogoutCurve, TextalignLeft } from "iconsax-react";
 import { Link, useLocation } from "react-router-dom";
 
+import useToggle from "../../../hooks/useToggle";
 import sidebarItems from "../../../config/sidebarItems";
 import { logoutUser } from "../../../redux/user/userActions";
 
@@ -13,33 +14,50 @@ const Sidebar = () => {
 
   const dispatch = useDispatch();
 
+  const [
+    sidebar__Ref,
+    openSidebar,
+    closeSidebar,
+    SidebarContainer,
+    toggleSidebar,
+  ] = useToggle(classes.show);
   return (
-    <aside className={classes.sidebar}>
-      {sidebarItems.map(({ key, title, url, icon }) => {
-        const className =
-          currentUrl == url
-            ? classes.sidebar__item_active
-            : classes.sidebar__item;
-
-        return (
-          <Link key={key} to={url} className={className}>
-            <span className="icon">
-              {createElement(icon, { size: "100%" })}
-            </span>
-            {title}
-          </Link>
-        );
-      })}
-      <button
-        onClick={() => dispatch(logoutUser())}
-        className={classes.sidebar__item}
-      >
-        <span className="icon">
-          <LogoutCurve size="100%" />
-        </span>
-        Logout
+    <>
+      <button className={`icon ${classes.sidebar__toggle}`} onClick={toggleSidebar}>
+        <TextalignLeft size="100%" />
       </button>
-    </aside>
+      <aside ref={sidebar__Ref} className={classes.sidebar}>
+        {sidebarItems.map(({ key, title, url, icon }) => {
+          const className =
+            currentUrl == url
+              ? classes.sidebar__item_active
+              : classes.sidebar__item;
+
+          return (
+            <Link
+              onClick={closeSidebar}
+              key={key}
+              to={url}
+              className={className}
+            >
+              <span className="icon">
+                {createElement(icon, { size: "100%" })}
+              </span>
+              {title}
+            </Link>
+          );
+        })}
+        <button
+          onClick={() => dispatch(logoutUser())}
+          className={classes.sidebar__item}
+        >
+          <span className="icon">
+            <LogoutCurve size="100%" />
+          </span>
+          Logout
+        </button>
+      </aside>
+    </>
   );
 };
 
