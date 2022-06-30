fba094ba6f5e693512468bda5d2392b1a664339b
AmirAbasi <amir.abasi.dev@gmail.com>
2022-06-30 22:32:04 +0430

    reafctor : ♻ cahnge structure product featrues

--- src/components/product/productFeatrues/ProductFeatrues.jsx
+++ src/components/product/productFeatrues/ProductFeatrues.jsx
@@ -2,18 +2,27 @@ import { useSelector } from "react-redux";
 
 import classes from "./productFeatrues.module.css";
 
-const ProductFeatrues = () => {
-  const { featrues } = useSelector((state) => state.product.product);
+const ProductFeatrues = ({ productFeatrues }) => {
+  const featrues =
+    productFeatrues ?? useSelector((state) => state.product.product.featrues);
 
   const featrueItems = Object.keys(featrues);
+
+  const classItemFeatrues = productFeatrues
+    ? classes.product__featrues_item_compare
+    : classes.product__featrues_item;
 
   return (
     <section className={classes.product__featrues}>
       <ul className={classes.product__featrues_ul}>
-        {featrueItems.map((title) => (
-          <li className={classes.product__featrues_item}>
-            <span className={classes.product__featrues_item_title}>{title}</span>
-            <span className={classes.product__featrues_item_value}>{featrues[title]}</span>
+        {featrueItems.map((title,index) => (
+          <li key={index} className={classItemFeatrues}>
+            <span className={classes.product__featrues_item_title}>
+              {title}
+            </span>
+            <span className={classes.product__featrues_item_value}>
+              {featrues[title]}
+            </span>
           </li>
         ))}
       </ul>
