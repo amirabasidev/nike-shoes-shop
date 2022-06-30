47f759548af48accf719f552df5816224aa458b5
AmirAbasi <amir.abasi.dev@gmail.com>
2022-06-30 21:46:24 +0430

    feat : ➕ add product to compare

--- src/components/product/productActions/ProductActions.jsx
+++ src/components/product/productActions/ProductActions.jsx
@@ -1,11 +1,35 @@
 import { Layer } from "iconsax-react";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+
+import { getItems, addItem } from "../../../services/LocalStorage";
 
 import classes from "./productActions.module.css";
 
 const ProductActions = () => {
+  const { product } = useSelector((state) => state.product);
+
+  const addProductToCompare = () => {
+    const compareItems = getItems("compare");
+
+    const findProduct = compareItems.some(
+      (productId) => productId === product.id,
+    );
+
+    if (findProduct) {
+      toast.warning("Product Is Compare");
+    } else {
+      addItem("compare", product.id);
+      toast.success("Add To Compare successfully !");
+    }
+  };
+
   return (
     <section className={classes.product__actions}>
-      <button className={`icon ${classes.product__actions_icon}`}>
+      <button
+        onClick={addProductToCompare}
+        className={`icon ${classes.product__actions_icon}`}
+      >
         <Layer size="100%" />
       </button>
     </section>
